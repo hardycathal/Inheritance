9153442443a108898933f4e1a07bd847af5e3808
hardycathal <hardyfcathal@gmail.com>
2023-12-05 13:17:38 +0000

    Created a new class called Person with getters/setters and two Constructors

---
+++ src/main/java/ie/atu/Person.java
@@ -0,0 +1,42 @@
+package ie.atu;
+
+class Person {
+    private String name;
+    private String address;
+    private String number;
+
+    public Person(){
+        this.name = " ";
+        this.address = " ";
+        this.number = " ";
+    }
+    public Person(String name, String address, String number) {
+        this.name = name;
+        this.address = address;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
